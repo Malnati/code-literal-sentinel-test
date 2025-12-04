8cdb09ce1b96e5eb63af14858c27f4cac04fa56b
Ricardo Malnati <35036961+Malnati@users.noreply.github.com>
2025-12-04 18:38:55 -0300

    Change hardcoded API_KEY value
    Updated API_KEY to a different hardcoded value.
    
    Signed-off-by: Ricardo Malnati <35036961+Malnati@users.noreply.github.com>

--- src/bad-code.js
+++ src/bad-code.js
@@ -1,7 +1,7 @@
 // src/bad-code.js
 
 // Hardcoded secrets (O clássico)
-const API_KEY = "sk-1234567890abcdef1234567890abcdef";
+const API_KEY = "sk-1234567890abcdef1234567890abcdefp";
 const DB_PASSWORD = 'super-secret-password-123';
 
 // Magic Strings (O Sentinela deve pegar isso)
